118ec86c193dab2c3e54692c9ac5879ff93716ff
harumich <talutalu.haru1486@gmail.com>
2022-01-21 02:36:12 +0900

    FlutterとFirebaseを連携
    android/app/build.grande
    android/build.grande
    
    lib/main.dart
    pubspec.lock
    pubspec.yaml
    
    途中、
    以下2箇所でエラーが発生。
    ①https://qiita.com/naotsune/items/8a45fb5436b50d3fef56
    ②その後、Execution failed for task ':app:compileDebugKotlin'. > Compilation error. See log for more detailsというエラーが起きたので、android/build.grandeの「classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"」を「classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10"」に変更して解決。

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:logintest/View/screen.dart';
 import 'package:logintest/View/start_up/login_page.dart';
 import 'package:logintest/View/time_line/time_line_page.dart';
 
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
