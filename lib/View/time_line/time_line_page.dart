8a69dd46eaa8504f7c21f969522c6f0bddc3378f
harumich <talutalu.haru1486@gmail.com>
2022-01-22 01:03:49 +0900

    タイムラインに投稿を表示させる

--- lib/View/time_line/time_line_page.dart
+++ lib/View/time_line/time_line_page.dart
@@ -4,6 +4,8 @@ import 'package:intl/intl.dart';
 import 'package:logintest/View/time_line/post_page.dart';
 import 'package:logintest/model/account.dart';
 import 'package:logintest/model/post.dart';
+import 'package:logintest/utils/firestore/posts.dart';
+import 'package:logintest/utils/firestore/users.dart';
 
 class TimeLinePage extends StatefulWidget {
   const TimeLinePage({Key? key}) : super(key: key);
@@ -13,30 +15,7 @@ class TimeLinePage extends StatefulWidget {
 }
 
 class _TimeLinePageState extends State<TimeLinePage> {
-  Account myAccount = Account(
-    id: '1',
-    name: 'Flutterラボ',
-    selfIntroduction: 'こんばんは',
-    userId: 'flutter_labo',
-    imagePath: 'https://pics.prcm.jp/5497864537d25/82527407/jpeg/82527407_480x599.jpeg',
-    createdTime: Timestamp.now(),
-    updatedTime: Timestamp.now(),
-  );
 
-  List<Post> postList = [
-    Post(
-      id: '1',
-      content: '初めまして',
-      postAccountId: '1',
-      createdTime: Timestamp.now()
-    ),
-    Post(
-        id: '2',
-        content: 'ご機嫌よう！',
-        postAccountId: '1',
-        createdTime: Timestamp.now()
-    ),
-  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,49 +25,83 @@ class _TimeLinePageState extends State<TimeLinePage> {
         backgroundColor: Theme.of(context).canvasColor,//bodyと同じ色の背景に
         elevation: 2,//appBarとbodyの境界線の太さ
       ),
-      body: ListView.builder(  //ListView.builderは同じようなものを繰り返し表示させていくのに使うwidget
-        itemCount: postList.length,  //いくつ表示する？
-        itemBuilder: (context, index) {  //何を表示させる
-          return Container(  //widget内のUIを設計
-            decoration: BoxDecoration(  //区切りを創る
-              border: index == 0 ? Border(  //区切りを創る
-                top: BorderSide(color: Colors.grey, width: 0),
-                bottom: BorderSide(color: Colors.grey, width: 0),
-              ) : Border(bottom: BorderSide(color: Colors.grey, width: 0),)
-            ),
-            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),  //widget内にゆとりを持たせる
-            child: Row(
-              children: [
-                CircleAvatar(  //CircleAvatar: 丸形のwidgetを表示させたいときに使う
-                  radius: 22,
-                  foregroundImage: NetworkImage(myAccount.imagePath),  //NetworkImage=インターネット上の画像
-                ),
-                Expanded(  //myAccount.name＆myAccount.userIdとDateFormatの間にスペースを創る
-                  child: Container(  //myAccount.name＆myAccount.userIdとDateFormatの間にスペースを創る
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,  //contentを真ん中から左に移動させる
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,  //myAccount.name＆myAccount.userIdとDateFormatの間にスペースを創る
+      body: StreamBuilder<QuerySnapshot>(
+        stream: PostFirestore.posts.orderBy('created_time', descending: true).snapshots(),
+        builder: (context, postSnapshot) {
+          if(postSnapshot.hasData) {
+            List<String> postAccountIds = [];
+            postSnapshot.data!.docs.forEach((doc) {
+              Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+              if(!postAccountIds.contains(data['post_account_id'])) {
+                postAccountIds.add(data['post_account_id']);
+              }
+            });
+            return FutureBuilder<Map<String, Account>?>(
+              future: UserFirestore.getPostUserMap(postAccountIds),
+              builder: (context, userSnapshot) {
+                if(userSnapshot.hasData && userSnapshot.connectionState == ConnectionState.done) {
+                  return ListView.builder(  //ListView.builderは同じようなものを繰り返し表示させていくのに使うwidget
+                    itemCount: postSnapshot.data!.docs.length,  //いくつ表示する？
+                    itemBuilder: (context, index) {//何を表示させる
+                      Map<String, dynamic> data = postSnapshot.data!.docs[index].data() as Map<String, dynamic>;
+                      Post post = Post(
+                        id: postSnapshot.data!.docs[index].id,
+                        content: data['content'],
+                        postAccountId: data['post_account_id'],
+                        createdTime: data['created_time']
+                      );
+                      Account postAccount = userSnapshot.data![post.postAccountId]!;
+                      return Container(  //widget内のUIを設計
+                        decoration: BoxDecoration(  //区切りを創る
+                            border: index == 0 ? Border(  //区切りを創る
+                              top: BorderSide(color: Colors.grey, width: 0),
+                              bottom: BorderSide(color: Colors.grey, width: 0),
+                            ) : Border(bottom: BorderSide(color: Colors.grey, width: 0),)
+                        ),
+                        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),  //widget内にゆとりを持たせる
+                        child: Row(
                           children: [
-                            Row(  //myAccount.name＆myAccount.userIdとDateFormatの間にスペースを創る
-                              children: [
-                                Text(myAccount.name, style: TextStyle(fontWeight: FontWeight.bold),),
-                                Text('@${myAccount.userId}', style: TextStyle(color: Colors.green)),
-                              ],
+                            CircleAvatar(  //CircleAvatar: 丸形のwidgetを表示させたいときに使う
+                              radius: 22,
+                              foregroundImage: NetworkImage(postAccount.imagePath),  //NetworkImage=インターネット上の画像
                             ),
-                            Text(DateFormat('M/d/yy').format(postList[index].createdTime!.toDate())),  // !='null'は絶対ないよ  //intlをインポートする  //String型に変更する
+                            Expanded(  //myAccount.name＆myAccount.userIdとDateFormatの間にスペースを創る
+                              child: Container(  //myAccount.name＆myAccount.userIdとDateFormatの間にスペースを創る
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,  //contentを真ん中から左に移動させる
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,  //myAccount.name＆myAccount.userIdとDateFormatの間にスペースを創る
+                                      children: [
+                                        Row(  //myAccount.name＆myAccount.userIdとDateFormatの間にスペースを創る
+                                          children: [
+                                            Text(postAccount.name, style: TextStyle(fontWeight: FontWeight.bold),),
+                                            Text('@${postAccount.userId}', style: TextStyle(color: Colors.green)),
+                                          ],
+                                        ),
+                                        Text(DateFormat('M/d/yy').format(post.createdTime!.toDate())),  // !='null'は絶対ないよ  //intlをインポートする  //String型に変更する
+                                      ],
+                                    ),
+                                    Text(post.content),  //contentを表示
+                                  ],
+                                ),
+                              ),
+                            )
                           ],
                         ),
-                        Text(postList[index].content),  //contentを表示
-                      ],
-                    ),
-                  ),
-                )
-              ],
-            ),
-          );
-        },
+                      );
+                    },
+                  );
+                } else {
+                  return Container();
+                }
+              }
+            );
+          } else {
+            return Container();
+          }
+
+        }
       ),
     );
   }
